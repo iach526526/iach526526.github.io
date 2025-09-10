f531bcb5a4dbd4b412d3992d2e362cef7bc730fb
Each Chen <info@iach.cc>
2025-09-10 23:50:13 +0800

    feat: top profile line

--- themes/nexmoe/layout/_partial/background.jsx
+++ themes/nexmoe/layout/_partial/background.jsx
@@ -21,16 +21,37 @@ module.exports = class extends Component {
                             <i class="mdui-icon nexmoefont icon-menu"></i>
                         </a>
                         <div class="mdui-toolbar-spacer"></div>
+
+                    </div>
+
+                    {/* 兩個 2×2 區塊 */}
+                    <div class="top-grid">
+                        <div class="quick-links card emfont-ChironGoRoundTC">
+                            <a href="/rss.xml" class="quick-btn">RSS</a>
+                            <a href="/about" class="quick-btn">關於本站</a>
+                            <a href="/bookshelf" class="quick-btn">書架</a>
+                            <a href="/blogroll" class="quick-btn">部落滾</a>
+                        </div>
+
+                        {/* 名片 */}
+                        <div class="profile-card card">
                         <a
                             href={url_for()}
                             title={config.author || config.title}
-                            class="mdui-btn mdui-btn-icon"
+                            class="profile-avatar"
                         >
                             <img
-                                src={theme.avatar }
+                                src={theme.avatar}
                                 alt={config.author || config.title}
                             />
                         </a>
+                            <h3 class="profile-name emfont-XiaoLaiMono">{config.author || "name"}</h3>
+                            <p class="profile-desc emfont-Tiejili">世界上大部分事，都沒有太大意義。<br/><br/>真理與熱愛除外。</p>
+                            {/* <div class="profile-icons">
+                                <a href="https://github.com/" target="_blank">github icon</a>
+                            </div> */}
+                            <div class="profile-action">➜</div>
+                        </div>
                     </div>
                 </div>
             </div>
