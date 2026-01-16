ac73794497e12e520d8816cc9568fb2f69997750
Each Chen <info@iach.cc>
2026-01-17 00:24:25 +0800

    feat: top tool bar

--- themes/nexmoe/layout/_partial/background.jsx
+++ themes/nexmoe/layout/_partial/background.jsx
@@ -23,39 +23,26 @@ module.exports = class extends Component {
                         <div class="mdui-toolbar-spacer"></div>
 
                     </div>
-
-                    {/* 兩個 2×2 區塊 */}
-                    <div class="top-grid">
-                        <div class="quick-links card emfont-ChironGoRoundTC">
-                            <a href="/rss.xml" class="quick-btn">RSS</a>
-                            <a href="/about" class="quick-btn">關於</a>
-                            <a href="/bookshelf" class="quick-btn">書架</a>
-                            <a href="/blogroll" class="quick-btn">部落滾</a>
-                        </div>
-
-                        {/* 名片 */}
-                        <div class="profile-card card">
-                        <a
-                            href={url_for()}
-                            title={config.author || config.title}
-                            class="profile-avatar"
-                        >
-                            <img
-                                src={theme.avatar}
-                                alt={config.author || config.title}
-                            />
-                        </a>
-                        <div class="profile-info">
-                            <h3 class="profile-name emfont-XiaoLaiMono">{config.author || "name"}</h3>
-                            <p class="profile-desc emfont-Tiejili">世界上大部分事，都沒有太大意義。<br/><br/>真理與熱愛除外。</p>
-                            {/* <div class="profile-icons">
-                                <a href="https://github.com/" target="_blank">github icon</a>
-                            </div> */}
-                            <div class="profile-action"><a href='/now'>➜</a></div>
-
-                        </div>
-                    </div>
-                    </div>
+                </div>
+                <div class="quick-links">
+                    <a href="/" class="quick-btn">回首頁</a>
+                    <a href="/achive" class="quick-btn">文章列表</a>
+                    <a href="/blogroll" class="quick-btn">部落滾</a>
+                    <a href="/bookshelf" class="quick-btn">書架</a>
+                    <a href="/about" class="quick-btn">關於</a>
+                    <a href="/rss.xml" class="quick-btn">RSS</a>
+                    <form id="search_form">
+                      <form id="search_form" class="quick-search" action="/search/" method="get">
+                        <input
+                        class="quick-search-input"
+                        id="search_value"
+                        name="q"
+                        type="search"
+                        placeholder="Search"
+                        autocomplete="off"
+                        />
+                    </form>
+                </form>
                 </div>
             </div>
         );
