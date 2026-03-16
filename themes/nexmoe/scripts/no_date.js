0186ee62eb110d975e40503a86caebfc57c1a523
Each Chen <info@iach.cc>
2026-03-17 05:17:06 +0800

    feat: filter stratage

--- themes/nexmoe/scripts/no_date.js
+++ themes/nexmoe/scripts/no_date.js
@@ -1,7 +1,7 @@
 hexo.extend.filter.register('before_post_render', function (data) {
   const categories = Array.isArray(data.categories) ? data.categories : [];
   const hasBookCategory = categories.some(cat => (cat && cat.name) ? cat.name === 'books' : cat === 'books');
-  const isBookSource = data.source.startsWith('bookshelf/') || data.source.startsWith('books/') || data.source.startsWith('_posts/books/');
+  const isBookSource = data.source.startsWith('bookshelf/') || data.source.startsWith('_posts/bookshelf/') ;
 
   if ((isBookSource || hasBookCategory) && !data.no_date) {
     data.no_date = true;
