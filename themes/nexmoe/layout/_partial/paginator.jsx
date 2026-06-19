42f91f8e96ecbd1068b48ec31e4fb63789d283b9
Each Chen <info@iach.cc>
2026-06-19 18:42:06 +0800

    feat: enhance pagination accessibility

--- themes/nexmoe/layout/_partial/paginator.jsx
+++ themes/nexmoe/layout/_partial/paginator.jsx
@@ -3,16 +3,19 @@ const { Component } = require('inferno');
 module.exports = class extends Component {
     render() {
         const { paginator } = this.props;
+        const pagination = paginator({
+            prev_text: '<span class="nexmoe-sr-only">上一頁</span><i class="nexmoefont icon-left" aria-hidden="true"></i>',
+            next_text: '<span class="nexmoe-sr-only">下一頁</span><i class="nexmoefont icon-right" aria-hidden="true"></i>',
+            escape: false
+        })
+            .replace('class="extend prev"', 'class="extend prev" title="上一頁" aria-label="上一頁"')
+            .replace('class="extend next"', 'class="extend next" title="下一頁" aria-label="下一頁"');
 
         return (
             <nav
                 class="nexmoe-page-nav"
                 dangerouslySetInnerHTML={{
-                    __html: paginator({
-                        prev_text: '<i class="nexmoefont icon-left"></i>',
-                        next_text: '<i class="nexmoefont icon-right"></i>',
-                        escape: false
-                    })
+                    __html: pagination
                 }}
             ></nav>
         );
